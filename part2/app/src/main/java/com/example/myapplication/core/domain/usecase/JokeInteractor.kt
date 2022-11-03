69d5a39a99c70747051b8dad45559807ecfc8444
reinhardjs <reinhardjsilalahi@gmail.com>
2022-11-04 00:00:30 +0700

    added JokeInteractor and its dependencies prior to the future usage of dependecy injection

---
+++ part2/app/src/main/java/com/example/myapplication/core/domain/usecase/JokeInteractor.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication.core.domain.usecase
+
+import com.example.myapplication.core.data.Resource
+import com.example.myapplication.core.domain.model.Joke
+import com.example.myapplication.core.domain.repository.IJokeRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class JokeInteractor @Inject constructor(private val jokeRepository: IJokeRepository) :
+    JokeUseCase {
+    override fun getRandomJoke(): Flow<Resource<Joke>> = jokeRepository.getRandomJoke()
+
+    override fun searchJokes(): Flow<Resource<List<Joke>>> = jokeRepository.searchJokes()
+}
