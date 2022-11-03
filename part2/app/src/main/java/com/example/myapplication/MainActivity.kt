e266d8e07dc331338373defb5739b901729639af
reinhardjs <reinhardjsilalahi@gmail.com>
2022-11-04 02:38:03 +0700

    uses recyclerview to show single random joke

--- part2/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ part2/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,56 +2,57 @@ package com.example.myapplication
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myapplication.core.data.Resource
+import com.example.myapplication.core.ui.JokeAdapter
+import com.example.myapplication.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private val mainViewModel: MainViewModel by viewModels()
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view: View = binding.root
+        setContentView(view)
 
-//        mainViewModel.randomJoke.observe(this) { joke ->
-//            if (joke != null) {
-//                when (joke) {
-//                    is Resource.Loading -> Toast.makeText(
-//                        applicationContext,
-//                        "Loading",
-//                        Toast.LENGTH_SHORT
-//                    ).show()
-//                    is Resource.Success -> Toast.makeText(
-//                        applicationContext,
-//                        joke.data?.value,
-//                        Toast.LENGTH_SHORT
-//                    ).show()
-//                    is Resource.Error -> Toast.makeText(
-//                        applicationContext,
-//                        "Error",
-//                        Toast.LENGTH_LONG
-//                    ).show()
-//                }
-//            }
-//        }
+        val jokeAdapter = JokeAdapter()
+        with(binding.rvJoke) {
+            layoutManager = LinearLayoutManager(applicationContext)
+            adapter = jokeAdapter
+        }
+
+        mainViewModel.randomJoke.observe(this) { joke ->
+            if (joke != null) {
+                when (joke) {
+                    is Resource.Loading -> binding.progressBar.visibility = View.VISIBLE
+                    is Resource.Success -> {
+                        binding.progressBar.visibility = View.GONE
+                        if (joke.data != null)
+                            jokeAdapter.setData(listOf(joke.data))
+                    }
+                    is Resource.Error -> {
+                        binding.progressBar.visibility = View.GONE
+                        binding.viewError.root.visibility = View.GONE
+                        binding.viewError.tvError.text = joke.message ?: "Something wrong"
+                    }
+                }
+            }
+        }
 
         mainViewModel.searchJokes("about").observe(this) { jokes ->
             if (jokes != null) {
                 when (jokes) {
-                    is Resource.Loading -> Toast.makeText(
-                        applicationContext,
-                        "Loading",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    is Resource.Success -> Toast.makeText(
-                        applicationContext,
-                        jokes.data.toString(),
-                        Toast.LENGTH_SHORT
-                    ).show()
+                    is Resource.Loading -> Log.d("MainActivity", "Loading Jokes Search Request")
+                    is Resource.Success -> Log.d("MainActivity", jokes.data.toString())
                     is Resource.Error -> Log.d("MainActivity", "Error : " + jokes.message)
                 }
             }
