233affaee44d9b9bd857ad14ec79333d64a5958f
reinhardjs <reinhardjsilalahi@gmail.com>
2022-11-03 23:56:51 +0700

    added JokeUseCase interface

---
+++ part2/app/src/main/java/com/example/myapplication/core/domain/usecase/JokeUseCase.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication.core.domain.usecase
+
+import com.example.myapplication.core.data.Resource
+import com.example.myapplication.core.domain.model.Joke
+import kotlinx.coroutines.flow.Flow
+
+interface JokeUseCase {
+    fun getRandomJoke(): Flow<Resource<Joke>>
+
+    fun searchJokes(): Flow<Resource<List<Joke>>>
+}
