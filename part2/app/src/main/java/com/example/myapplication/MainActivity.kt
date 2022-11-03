6ea1608089ca15def213642f2740bc9e605f38e3
reinhardjs <reinhardjsilalahi@gmail.com>
2022-11-04 02:03:53 +0700

    implement the simple usage of viewModel call in main activity

--- part2/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ part2/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,13 +1,41 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.core.data.Resource
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val mainViewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainViewModel.randomJoke.observe(this) { joke ->
+            if (joke != null) {
+                when (joke) {
+                    is Resource.Loading -> Toast.makeText(
+                        applicationContext,
+                        "Loading",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    is Resource.Success -> Toast.makeText(
+                        applicationContext,
+                        joke.data?.value,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    is Resource.Error -> Toast.makeText(
+                        applicationContext,
+                        "Error",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        }
     }
 }
