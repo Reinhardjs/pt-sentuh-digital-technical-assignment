acedc30fa682015c4fa21fe6f01cf8d1b4f17306
reinhardjs <reinhardjsilalahi@gmail.com>
2022-11-03 23:52:21 +0700

    added JokeRepository interface contract and its dependencies

---
+++ part2/app/src/main/java/com/example/myapplication/core/domain/repository/IJokeRepository.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication.core.domain.repository
+
+import com.example.myapplication.core.domain.data.Resource
+import com.example.myapplication.core.domain.model.Joke
+import kotlinx.coroutines.flow.Flow
+
+interface IJokeRepository {
+    fun getRandomJoke(): Flow<Resource<Joke>>
+
+    fun searchJokes(): Flow<Resource<List<Joke>>>
+}
